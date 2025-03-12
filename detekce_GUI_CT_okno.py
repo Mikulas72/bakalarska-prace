71ad8c1dedfebff6e21e107d2fa38cd9c3054291
Mikuláš Mičkal <240393@vutbr.cz>
2025-03-12 11:34:49 +0100

    Pomocný skript pro GUI s aplikací CT okna - skoro hotovo s popisky

--- detekce_GUI_CT_okno.py
+++ detekce_GUI_CT_okno.py
@@ -1,11 +1,10 @@
+# Knihovny potřebné pro chod postprocessingové aplikace CT okna
 import tkinter as tk
-from tkinter import *
-
 import SimpleITK as sitk
 import numpy as np
 
 
-
+# Blok kódu s funkcemi pro správný chod postprocessingové aplikace CT okna
 # Funkce, která umožní uživateli výběr požadovaného CT okna
 def vyber_CT_okna(pocet_spusteni):
     # Vytvoření okna, které umožní uživateli výběr CT okna
@@ -24,6 +23,7 @@ def vyber_CT_okna(pocet_spusteni):
     mezera = tk.Label(dotaz_ct_okno, padx=odsazeni)
     mezera.grid(row=1, column=0, columnspan=3, sticky=sticky_parametr_sloupec)
 
+
     # Tlačítka pro výběr požadovaného mozkového okna
     tlacitko_mozkove_okno = tk.Button(dotaz_ct_okno, text="Mozkové okno", anchor=kotva, font=font_tlacitek, command=lambda: (dotaz_ct_okno.destroy(), nastav_CT_okno("mozek", pocet_spusteni)))
     tlacitko_mozkove_okno.grid(row=2, column=0, sticky=sticky_parametr_sloupec, padx=odsazeni, pady=odsazeni)
@@ -40,6 +40,7 @@ def vyber_CT_okna(pocet_spusteni):
 
     mezera = tk.Label(dotaz_ct_okno, padx=odsazeni)
     mezera.grid(row=4, column=0, columnspan=3, sticky=sticky_parametr_sloupec)
+
 
     # Resetovací tlačítko (odstrani z obrazu všechny CT okna)
     tlacitko_reset = tk.Button(dotaz_ct_okno, text="Reset", anchor=kotva, font=font_tlacitek, command=lambda: (dotaz_ct_okno.destroy(), nastav_CT_okno("reset", pocet_spusteni)))
@@ -104,36 +105,42 @@ def nastav_CT_okno(okno, pocet_spusteni):
         # Zavolá se funkce, která uživateli oznámí, že se CT okno aplikovalo
         return odpoved_po_uprave(False)
 
-
+# Tato funkce vytvoří oznamovací okno, že byl na obraz aplikováno vybrané CT okno nebo že z něho bylo odstraněno
 def odpoved_po_uprave(reset):
+    # Vytvoření oznamovacího okna (nadpisu a rozměrů okna)
     dotaz_okno = tk.Tk()
     dotaz_okno.title("Upravení obrazu")
     dotaz_okno.geometry("300x50")
 
+    # Pokud byla vstupní hodnota False, znamená to, že si užýivatel přeje aplikovat na obraz CT okno a v oznamovacím okně se vypíše příslušné oznámení
     if reset == False:
         dotaz = tk.Label(dotaz_okno, text="Na obraz bylo úspěšně aplikováno vybrané CT okno.", anchor="center")
+
+    # Pokud byla vstupní hodnota True, znamená to, že si užýivatel přeje odstranit z obrazu CT okno a v oznamovacím okně se vypíše příslušné oznámení
     else:
         dotaz = tk.Label(dotaz_okno, text="Z obrazu bylo úspěšně odebráno CT okno.", anchor="center")
 
     dotaz.pack()
 
-
+# Tato funkce vytváří nabídkové okno, které uživateli umožní vložit vlastní parametry pro CT okno
 def dotaz_ct_okno_vlastni(pocet_spusteni):
+    # Základní parapetry a vytvoření nabídkového okno (nadpis, rozměry okna a parametry textu)
     dotaz_vlastni_ct_okno = tk.Tk()
     dotaz_vlastni_ct_okno.title("Vlastní CT okno")
-    dotaz_vlastni_ct_okno.geometry("510x150")
+    dotaz_vlastni_ct_okno.geometry("510x170")
     odsazeni = 5
     sticky_parametr_sloupec = "ew"
     var = tk.StringVar()
     var.set("A")
 
+    # Vytvoření a umístění textu pro navedení uživatele
     nadpis = tk.Label(dotaz_vlastni_ct_okno, text="Nastavte si požadované parametry vlastního CT okna.", font=("Arial", 12))
     nadpis.grid(row=0, column=0, columnspan=5, sticky=sticky_parametr_sloupec, padx=odsazeni)
 
     mezera = tk.Label(dotaz_vlastni_ct_okno, padx=odsazeni)
     mezera.grid(row=1, column=0, columnspan=5, sticky=sticky_parametr_sloupec)
 
-
+    # Vytvoření a umístění nadpisů a vstupů pro zadání středu a šířky vlastního CT okna
     popis_stred = tk.Label(dotaz_vlastni_ct_okno, text="Střed okna:")
     popis_stred.grid(row=2, column=0, sticky=sticky_parametr_sloupec)
     vstup_stred = tk.Entry(dotaz_vlastni_ct_okno, validate="key", validatecommand=(dotaz_vlastni_ct_okno.register(overeni_vstupu), "%P"))
@@ -146,6 +153,7 @@ def dotaz_ct_okno_vlastni(pocet_spusteni):
     mezera = tk.Label(dotaz_vlastni_ct_okno, pady=5)
     mezera.grid(row=2, column=2, rowspan=3, sticky="sn")
 
+    # Vytvoření a umístění nadpisů a vstupů pro zadání maxima a minima vlastního CT okna
     popis_max = tk.Label(dotaz_vlastni_ct_okno, text="Horní hranice okna:")
     popis_max.grid(row=2, column=3, sticky=sticky_parametr_sloupec)
     vstup_max = tk.Entry(dotaz_vlastni_ct_okno, validate="key", validatecommand=(dotaz_vlastni_ct_okno.register(overeni_vstupu), "%P"))
@@ -158,50 +166,66 @@ def dotaz_ct_okno_vlastni(pocet_spusteni):
     mezera = tk.Label(dotaz_vlastni_ct_okno, padx=odsazeni)
     mezera.grid(row=4, column=0, columnspan=5, sticky=sticky_parametr_sloupec)
 
-    tlacitko_spustit_A = tk.Button(dotaz_vlastni_ct_okno, text="Použít šírku a výšku okna", font=("Arial", 10, "bold"), command=lambda: (zpracuj_vlastni_ct_okno(vstup_stred.get(), vstup_sirka.get(), "", "", True, pocet_spusteni), dotaz_vlastni_ct_okno.destroy()))
-    tlacitko_spustit_A.grid(row=5, column=0, columnspan=2, sticky=sticky_parametr_sloupec, padx=odsazeni, pady=odsazeni)
-    tlacitko_spustit_B = tk.Button(dotaz_vlastni_ct_okno, text="Použít maximum a minimum okna", font=("Arial", 10, "bold"), command=lambda: (zpracuj_vlastni_ct_okno("", "", vstup_minimum.get(), vstup_max.get(), False, pocet_spusteni), dotaz_vlastni_ct_okno.destroy()))
-    tlacitko_spustit_B.grid(row=5, column=3, columnspan=2, sticky=sticky_parametr_sloupec, padx=odsazeni, pady=odsazeni)
-    tlacitko_smazat = tk.Button(dotaz_vlastni_ct_okno, text="Smazat", font=("Arial", 10, "bold"), command=lambda: (vstup_stred.delete(0, tk.END), vstup_sirka.delete(0, tk.END), vstup_max.delete(0, tk.END), vstup_minimum.delete(0, tk.END)))
-    tlacitko_smazat.grid(row=5, column=2, sticky=sticky_parametr_sloupec, padx=odsazeni, pady=odsazeni)
+    # Vytvoření a umístění upozornění pro uživatele, že lze zadávat pouze číselné hodnoty
+    text_upozorneni = tk.Label(dotaz_vlastni_ct_okno, text="Upozornění: Zadávejte pouze číselné hodnoty.")
+    text_upozorneni.grid(row=5, column=0, columnspan=5, sticky=sticky_parametr_sloupec)
 
+    # Tlačítka pro potvrzení zadaných údajů a tlačítko pro vymazání vstupů (kdyby si to uživatel rozmyslel)
+    tlacitko_spustit_A = tk.Button(dotaz_vlastni_ct_okno, text="Použít šírku a výšku okna", font=("Arial", 10, "bold"), command=lambda: (zpracuj_vlastni_ct_okno(vstup_stred.get(), vstup_sirka.get(), "", "", True, pocet_spusteni), dotaz_vlastni_ct_okno.destroy()))
+    tlacitko_spustit_A.grid(row=6, column=0, columnspan=2, sticky=sticky_parametr_sloupec, padx=odsazeni, pady=odsazeni)
+    tlacitko_spustit_B = tk.Button(dotaz_vlastni_ct_okno, text="Použít maximum a minimum okna", font=("Arial", 10, "bold"), command=lambda: (zpracuj_vlastni_ct_okno("", "", vstup_minimum.get(), vstup_max.get(), False, pocet_spusteni), dotaz_vlastni_ct_okno.destroy()))
+    tlacitko_spustit_B.grid(row=6, column=3, columnspan=2, sticky=sticky_parametr_sloupec, padx=odsazeni, pady=odsazeni)
+    tlacitko_smazat = tk.Button(dotaz_vlastni_ct_okno, text="Smazat", font=("Arial", 10, "bold"), command=lambda: (vstup_stred.delete(0, tk.END), vstup_sirka.delete(0, tk.END), vstup_max.delete(0, tk.END), vstup_minimum.delete(0, tk.END)))
+    tlacitko_smazat.grid(row=6, column=2, sticky=sticky_parametr_sloupec, padx=odsazeni, pady=odsazeni)
+
+    # Hlavní smyčka nabídkového okna
     dotaz_vlastni_ct_okno.mainloop()
 
-
+# Funkce aplikující zadané hodnoty uživatelem pro vlastní CT okno na daný obraz
 def zpracuj_vlastni_ct_okno(stred, sirka, minimum, maximum, vyber, pocet_spusteni):
+    # Podle počtu spuštění postprocessingu se vybere, zda se může pro úpravy využít současný obrázek nebo se musí vyvolat záloha
+    # (Zabraňuje kumulativnímu použití CT oken na již upravené obrazy)
     if pocet_spusteni == 0:
         obrazek_puvodni = sitk.ReadImage("upload_image.nii")
     else:
         obrazek_puvodni = sitk.ReadImage("puvodni_ct_obrazek.nii")
 
+    # Nahraný obraz se převede na numpy matici a nastaví se jeho datový typ
     obrazek_pole = sitk.GetArrayFromImage(obrazek_puvodni).astype(np.float32)
 
+    # Pokud je vstupní hodnota True, tak to znamená, že si uživatel přeje nastavit CT okno podle středu okna a jeho šířky
     if vyber == True:
         stred = int(stred)
         sirka = int(sirka)
         minimum = stred - (sirka / 2)
         maximum = stred + (sirka / 2)
 
+    # Pokud je vstupní hodnota False, tak to znamená, že si uživatel přeje nastavit CT okno podle maxima a minima
     else:
         maximum = int(maximum)
         minimum = int(minimum)
 
+    # Obraz se ořízne (tedy nastaví se vlastní CT okno) a provede se normalizace
     obrazek_pole = np.clip(obrazek_pole, minimum, maximum)
     obrazek_pole = 255.0 * (obrazek_pole - minimum) / (maximum - minimum)
 
+    # Obraz se převede zpět z numpy matice a uloží se do paměti GUI
     obrazek_zmeneny = sitk.GetImageFromArray(obrazek_pole)
     sitk.WriteImage(obrazek_zmeneny, "upload_image.nii")
 
+    # Vypíše se upozornění pro uživatele, že postprocessingové úpravy byli dokončeny
     odpoved_po_uprave(False)
 
-
-
-
+# Funke kontroluje, jaké vstupy zadává uživatel do vstupů pro parametry vlastního CT okna
 def overeni_vstupu(text_vstup):
+    # Pokud uživatel zadá číslo, pomlčku (mínus) nebo nezadá nic, tak funkce vstup povolí
     if text_vstup.isdigit() or text_vstup == "" or text_vstup == "-":
         return True
+
+    # Pokud uživatel zadá záporné číslo, tak funkce vstup povolí
     elif text_vstup.lstrip("-").isdigit():
         return True
+
+    # Pokud ale uživatel zadá něco jiného, tak funkce vstup zamítne
     else:
         return False
-
